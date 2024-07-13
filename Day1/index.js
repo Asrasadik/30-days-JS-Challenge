7939576a834afe0664d7331b13629f862f707902
Asra <47492644+Asrasadik@users.noreply.github.com>
2024-07-13 16:12:28 +0000

    Day 1🍥

---
+++ Day1/index.js
@@ -0,0 +1,23 @@
+var iNum = 2;
+console.log(iNum);
+let sChar = 'Apple';
+console.log(sChar);
+////////////////////
+const bIsTrue = true;
+console.log(bIsTrue);
+////
+let arr = [2, 'Hello', true];
+let obj = {
+	name: 'Zoe',
+	Id: 4764,
+	city: 'Tsegie',
+};
+console.log(typeof iNum, typeof sChar, typeof bIsTrue, typeof arr, typeof obj);
+//in JavaScript, arrays are a type of object
+///////////
+let  dummy = 2
+console.log(dummy);
+dummy = "Orange"
+console.log(dummy);
+//////
+//bIsTrue = false;//typeerror
